81190bca0d683cbe4655106c5cc7539642e3c719
RV <rv893302@gmail.com>
2026-03-14 22:32:42 +0530

    Add gotoxy function

--- Project.c
+++ Project.c
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 // CROSS-PLATFORM SLEEP FUNCTION
 #ifdef _WIN32
@@ -11,7 +12,23 @@ void sleep_ms(int ms) { Sleep(ms); }
 void sleep_ms(int ms) { usleep(ms * 1000); }
 #endif
 
+// FUNTIONS PROTOTYPES
+void gotoxy(int x, int y);
+
+// MAIN FUNCTION
 int main() {
     // Write Code
     return 0;
+}
+
+// FUNCTION DEFINITIONS
+
+// =========================MOVE CURSOR TO (X, Y) POSITION=========================
+
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
